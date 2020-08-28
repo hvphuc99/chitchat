3daea174b8d5c34c4d312fbfac78e1ce3cf3112b
Phuc Hoang <hvphuc081299@gmail.com>
2020-08-28 16:40:41 +0700

    add NotFound

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CssBaseline, ThemeProvider } from "@material-ui/core";
 
 import ForgotPassword from "features/Auth/pages/ForgotPassword";
 import Login from "features/Auth/pages/Login";
+import NotFound from "components/NotFound";
 import React from "react";
 import SignUp from "features/Auth/pages/SignUp";
 import theme from "theme";
@@ -16,9 +17,10 @@ function App() {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/forgotpassword" component={ForgotPassword} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/forgotpassword" component={ForgotPassword} />
+            <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
       </ThemeProvider>
